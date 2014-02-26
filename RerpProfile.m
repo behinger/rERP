e5c6539f37cf57b3ffcf41e07d57a00bc666f2f3
mattb243 <mattb243@gmail.com>
2014-02-26 11:36:04 -0800

    Changed default profile

--- RerpProfile.m
+++ RerpProfile.m
@@ -311,7 +311,7 @@ classdef RerpProfile < matlab.mixin.Copyable
                 'lambda',[1 1],...specify lambda to use if cross validation is disabled
                 'cross_validate_enable', 1,...do a grid search for lambda 
                 'num_xvalidation_folds', 5,...number of folds to use during cross validation 
-                'num_grid_zoom_levels', 1,...number of levels of grid search zooming
+                'num_grid_zoom_levels', 2,...number of levels of grid search zooming
                 'num_grid_points', 10,...number of points to sample at each grid level
                 'first_phase_lambda', [0 logspace(log10(1e-6), log10(1e8), 20)]',... the initial sample space for grid search
                 'elasticnet_quick_zoom', 1,...for ElasticNet (L1+L2) penalty. find optimal lambda1 and lambda2 serpately first, then 2D grid search around those values
