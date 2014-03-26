a3bbc4cae322df8acc419f524b092845facef862
mattb243 <mattb243@gmail.com>
2014-03-26 09:13:09 -0700

    Make pop_rerp_study more efficient by saving computed artifact indexes

--- pop_rerp_study.m
+++ pop_rerp_study.m
@@ -77,32 +77,39 @@ if isempty(rerp_profiles)||isempty(eeg_dataset_paths)||~exitcode
     return;
 end
 
-% Run study one dataset at a time, for all profiles. If profile loaded did not specify to
+% Run study, one dataset at a time, for all profiles. If profile loaded did not specify to
 % analyze all components or chans, then only those specified in the profile
 % will be analyzed. 
-rerp_results=cell(length(eeg_dataset_paths));
+rerp_results=cell(length(eeg_dataset_paths), length(rerp_profiles));
 for i=1:length(eeg_dataset_paths)
-    EEG=pop_loadset(eeg_dataset_paths{i});    
+    EEG=pop_loadset(eeg_dataset_paths{i});
+    artifact_name='';
+    artifact=[]; 
     for j=1:length(rerp_profiles)
-        disp(['pop_rerp_study: processing dataset ' num2str(i) '/' num2str(length(eeg_dataset_paths)) ': ' EEG.filename ', profile ' num2str(j) '/' num2str(length(rerp_profiles))]);
 
-        if (~rerp_profiles(i).settings.type_proc)&&isempty(EEG.icaact)
+        disp(['pop_rerp_study: processing dataset ' num2str(i) '/' num2str(length(eeg_dataset_paths)) ': ' EEG.filename ', profile ' num2str(j) '/' num2str(length(rerp_profiles)) ': ' rerp_profiles(j).name ]);
+
+        if (~rerp_profiles(j).settings.type_proc)&&isempty(EEG.icaact)
             EEG.icaact=eeg_getica(EEG);
         end
-
-        %Make a profile specific to this dataset based on the profile we
-        %selected. 
-        new_prof=RerpProfile(EEG, rerp_profiles(j));
-        rerp_results{i,j}=pop_rerp(EEG, new_prof);
+        
+        %Save us from recomputing artifact indexes for multiple profiles
+        this_profile = RerpProfile(EEG, rerp_profiles(j));
+        this_profile.computed_artifact_indexes = artifact;
+        this_profile.computed_artifact_indexes_function_name = artifact_name; 
+        
+        rerp_results{i,j}=pop_rerp(EEG, this_profile);
+        artifact = rerp_results{i,j}.rerp_profile.computed_artifact_indexes; 
+        artifact_name = rerp_results{i,j}.rerp_profile.computed_artifact_indexes_function_name; 
     end
 end
 
 rerp_result_study=cell(1,length(rerp_profiles)); 
 for i=1:length(rerp_profiles)
     % Create the RerpStudyResult object for each profile
-    rerp_result_study{i} = RerpResultStudy(rerp_results(:,j));
+    rerp_result_study{i} = RerpResultStudy([rerp_results{:, i}]);
 end
-rerp_result_study=rerp_result_study(cell2mat(cellfun(@(x) ~isempty(x), rerp_result_study))); 
+rerp_result_study=[rerp_result_study{:}]; 
 
 
 
