300e8b6c9e08109ebba324ef347b86e0e5144f95
mattb243 <mattb243@gmail.com>
2014-03-27 06:18:47 -0700

    Update script examples

--- rerp_example.m
+++ rerp_example.m
@@ -1,59 +1,69 @@
-%% Startup script: load RSVP dataset first 
-%http://headit-beta.ucsd.edu/studies/8004e8ee-a236-11e2-b5e7-0050563f2612
-path_to_dataset=''; 
-EEG=pop_loadset(path_to_dataset);
+%% The primary source of documentation for functions and classes is the Matlab help system:
+doc pop_rerp;
+doc pop_rerp_study;
+doc RerpProfile;
+doc RerpResult;
+doc RerpResultStudy;
 
-% Add HED tags to EEG struct
-% For GUI tagging, use EEG = tageeg(EEG); requires ctagger plugin (http://visual.cs.utsa.edu/software/ctagger)
-for i=1:length(EEG.event)
-    switch EEG.event(i).type
-        case 1
-            EEG.event(i).hedTag='stimulus/visual;stimulus/onset;stimulus/expected';
-        case 2 
-            EEG.event(i).hedTag='stimulus/visual;stimulus/onset;stimulus/expected/target';
-        case 4
-            EEG.event(i).hedTag='response/button press';
-        case 5
-            EEG.event(i).hedTag='response/button press';
-        case 6
-            EEG.event(i).hedTag='time-locked event';
-        case 16
-            EEG.event(i).hedTag='stimulus/visual;stimulus/onset;stimulus/instruction/fixate';
-        case 32
-            EEG.event(i).hedTag='stimulus/visual;stimulus/feedback/correct';
-        case 64
-            EEG.event(i).hedTag='stimulus/visual;stimulus/feedback/incorrect';
-        case 129
-            EEG.event(i).hedTag='stimulus/visual;stimulus/expected';
-    end 
-end
 
-%% This will get you started with the main setup GUI for the first time
+%% Main setup GUI
 rerp_result_study = pop_rerp_study('eeg', EEG); 
 
+%% Create and edit a new profile for the current dataset
+rerp_profile = RerpProfile(EEG);
+rerp_profile_gui(rerp_profile); 
 
 %% If you have profiles saved to disk, you can load the ones you want and run them against new datasets
 rerp_profiles = RerpProfile.loadRerpProfile;
 
 %% You can easily run multiple datasets against multiple profiles
-[STUDY ALLEEG] = pop_loadstudy(path_to_study); 
-rerp_result_study = pop_rerp_study('eeg', EEG, 'rerp_profiles', rerp_profiles, 'force_gui', 1);  
+rerp_result_study = pop_rerp_study('eeg', EEG, 'rerp_profiles', rerp_profiles); 
+
 %or
-%rerp_result_study = pop_rerp_study('eeg', ALLEEG, 'rerp_profiles', rerp_profiles, 'force_gui', 1);
+%rerp_result_study = pop_rerp_study('eeg', ALLEEG, 'rerp_profiles', rerp_profiles);
+
 %or if STUDY is populated
-%rerp_result_study = pop_rerp_study('study', STUDY, 'rerp_profiles', rerp_profiles, 'force_gui', 1);
+%rerp_result_study = pop_rerp_study('study', STUDY, 'rerp_profiles', rerp_profiles);
 
-%Run analysis with a single dataset and a single profile
-rerp_result = pop_rerp(EEG(1), rerp_profiles{1}, 'force_gui', 1);
+%Toolbox will parallelize over channels or components automatically using
+%parfor. 
 
-%Run analysis with a single dataset and a single profile
-rerp_result = pop_rerp(EEG(1), rerp_result_study{1}.rerp_profile, 'force_gui', 1);
+%% Run analysis with a single dataset and a single profile
+rerp_result = pop_rerp(EEG(1), rerp_profiles(1));
 
-%Plot results. This will look in the default directory /results and load
-%all .rerp_result files.
-rerp_result_gui; 
+%% Plot results. This will look in the default directory /results and load all .rerp_result files.
+rerp_result_gui;
 
-%Modify an RerpProfile object, but don't run analysis.
-rerp_profile_gui(rerp_profiles{1});
+%or
+%rerp_result_gui('results', rerp_result_study.result); 
 
+%or
+%rerp_result_gui('results', rerp_result);
+
+%% Create a profile for each dataset from template, compute artifact indexes for all datasets and save for future use 
+%It might be useful to save all the profiles and the .study file in a
+%single directory. 
+
+path2profiles=fullfile(RerpProfile.rerp_path, 'profiles', 'example'); 
+template_profile = RerpProfile(EEG);
+rerp_profile_gui(template_profile);
+
+all_profiles=[]; 
+parfor i=1:length(ALLEEG)
+    all_profiles(i) = RerpProfile(ALLEEG(i), template_profile);
+    all_profiles(i).compute_artifact_indexes(EEG); 
+    all_profiles(i).saveRerpProfile('path', fullfile(path2profiles, [this_eeg.setname '.rerp_profile']));  
+end
+
+%Get the profiles you just computed in another session. This loads all
+%the profiles in the folder 
+all_profiles = RerpProfile.loadRerpProfile('path', path2profiles); 
+
+%Run the profiles against the datasets they were derived from. This computes channels/comps in parallel, if 
+%you run matlabpool with more than 2 workers, or inside a batch job with many workers. 
+for i=1:length(ALLEEG)
+    all_results(i) = pop_rerp(ALLEEG(i), all_profiles(i)); 
+end 
+
+rerp_result_study = RerpResultStudy(all_results); 
 
