2c6654ca51faa3021720c6568949ba8c52a23a98
mattb243 <mattb243@gmail.com>
2014-04-21 18:39:33 +0530

    documentation

--- eegplugin_rerp.m
+++ eegplugin_rerp.m
@@ -1,8 +1,8 @@
-%Add rERP toolbox functionality to EEGLAB GUI 
+%eegplugin_rerp: Add rERP toolbox functionality to EEGLAB GUI. 
+%   Operates on CONTINUOUS DATA only: will be greyed out if epoched data is loaded 
 function eegplugin_rerp( fig, trystr, catchstr )
 toolsmenu = findobj(fig, 'tag', 'tools');
 submenu = uimenu( toolsmenu, 'label', 'rERP','separator','on','userdata','startup:on;epoch:off;study:on');
-
 uimenu( submenu, 'label', 'Run analysis', 'callback', 'pop_rerp_study(''eeg'', ALLEEG);', 'userdata','study:on;startup:on');
 uimenu( submenu, 'label', 'Plot results', 'callback', 'rerp_result_gui;','userdata','study:on;startup:on');
 end
