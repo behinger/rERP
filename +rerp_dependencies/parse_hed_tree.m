7964a2e871cf916d9d016e0f15108414593c14c2
mattb243 <mattb243@gmail.com>
2014-09-04 22:56:31 -0400

    rewriting parse_hed_tree with makeContinousVar

--- +rerp_dependencies/parse_hed_tree.m
+++ +rerp_dependencies/parse_hed_tree.m
@@ -7,14 +7,16 @@ import rerp_dependencies.*
 
 tags = hed_tree.uniqueTag;
 ids = hed_tree.originalHedStringId;
+[continuous_var, tags, ids] = make_continuous_var(tags, ids);
+available_continuous_tags = [continuous_var{:}.name];
 disp('parse_hed_tree: removing exclude/seperator/continuous tags');
 
 %Remove exclude_tags, seperator_tags and continuous_tags
 k=1;
-remove=[];
+remove=[]; 
 for i = 1:length(tags)
-    this_tag = tags{i};
-    
+    this_tag = tags{i};  
+            
     for j=1:length(exclude_tags)
         if strcmpi(this_tag, exclude_tags{j})
             remove(k) = i;
@@ -38,12 +40,10 @@ for i = 1:length(tags)
 end
 
 %Remove excluded, continuous and seperator tags from the list
-idx = setdiff(1:length(ids), remove);
+idx = setdiff(1:length(ids), unique(remove));
 ids = ids(idx);
 tags = tags(idx);
 
-%Extract continuous variables
-[continuous_var, tags, ids] = makeContinuousVar(tags, ids, continuous_tags);
 
 %Form context groups
 [context_group, tags, ids] = makeContextGroup(hed_tree, tags, ids, seperator_tags);
@@ -76,19 +76,31 @@ for i=1:len_ids
     
     for j = (i+1):len_ids
         %Tags with identical ids are duplicate
-        if isequal(id_list(j),this_id)
-            marked_tags{end+1} = ['*   ' tag_list{i} '   *'];
-            marked_ids(end+1) = id_list(i);
-            ridx = zeros(size(id_list));
-            ridx(i) = 1;
+        if isequal(id_list{j}, this_id{:})
+
+            node_seq = regexp(RerpTagList.strip_brackets(tag_list(i)), '[/]', 'split');
             
-            %Remove one duplicate tag
-            id_list = id_list(~ridx);
-            tag_list = tag_list(~ridx);
-            
-            %Go one level deeper in recursion
-            [short_tags, short_ids, marked_tags, marked_ids] = remove_redundant_tags(tag_list, id_list, marked_tags, marked_ids);
-            return;
+            if ~isempty(node_seq)
+                node_seq=node_seq{1}; 
+                
+                %Don't remove continuous tags with same latencies: magnitude
+                %can be different even if latency is the same
+                if ~any(strcmp(strjoin(node_seq(1:(end-1)),'/'), continuous_tags))
+                    marked_tags{end+1} = ['*   ' tag_list{i} '   *'];
+                    marked_ids(end+1) = id_list(i);
+                    
+                    ridx = zeros(size(id_list));
+                    ridx(i) = 1;
+                    
+                    %Remove one duplicate tag
+                    id_list = id_list(~ridx);
+                    tag_list = tag_list(~ridx);
+                    
+                    %Go one level deeper in recursion
+                    [short_tags, short_ids, marked_tags, marked_ids] = remove_redundant_tags(tag_list, id_list, marked_tags, marked_ids);
+                    return;
+                end
+            end
         end
     end
 end
@@ -135,8 +147,8 @@ for i=1:length(seperator_tag)
         end
     end
     
-    %We don't want the context groups to affect each other's children
-    %(keeps them seperated).
+    %We don't want the context groups to affect each other's children,
+    %keep them seperated.
     rem_tags = {rem_tags{:} tags{remove}};
     
     % Determine which tags to replicate in the context groups (co-occurring
@@ -162,13 +174,14 @@ end
 sub_tags_idx = setdiff(1:length(tags), rem_idx);
 sub_tags = tags(sub_tags_idx);
 
+
 for i=1:length(context_group)
     this_group = context_group{i};
     
     if ~isempty(this_group)
         [stripped_hit_tags, this_idx, ~] = intersect(RerpTagList.strip_affected_brackets(sub_tags), this_group.affected_tags);
         hit_tags = sub_tags(this_idx);
-        
+
         %Find the affected ids which are included for each child
         for j=1:length(this_group.children)
             this_child = this_group.children(j);
@@ -195,56 +208,4 @@ for i=1:length(context_group)
     end
 end
 
-end
-
-function [continuous_var, tags, ids] = makeContinuousVar(tags, ids, continuous_tags)
-import rerp_dependencies.*
-continuous_var=cell(size(continuous_tags));
-
-k=1;
-remove=[];
-for i=1:length(continuous_tags)
-    fprintf('parse_hed_tree: creating continuous variable %d/%d\n', i,length(continuous_tags));
-    
-    this_continuous_tag = continuous_tags{i};
-    node_seq = regexp(this_continuous_tag, '[/]', 'split');
-    
-    if ~isempty(node_seq) && isempty(node_seq{1})
-        node_seq(1)=[];
-    end
-    
-    this_continuous_var=[];
-    for j=1:length(tags)
-        this_unique_tag = tags{j};
-        this_node_seq = regexp(this_unique_tag, '[/]', 'split');
-        
-        %Child of the continuous tag will have a longer node sequence
-        if length(this_node_seq) > length(node_seq)
-            
-            %Check if this tag is a child of the seperator tag, store as context group
-            if ~nnz(~strcmpi(this_node_seq(1:length(node_seq)), node_seq))
-                tags{j} = ['[   ' this_unique_tag '   ]'];
-                this_original_id = ids{j};
-                %Parse the last node as a continuous variable
-                val = str2double(this_node_seq(end));
-                val = repmat(val, 1,length(this_original_id));
-                
-                this_continuous_var(k).val = val;
-                this_continuous_var(k).ids = this_original_id;
-                k=k+1;
-                remove(k)=j;
-            end
-        end
-    end
-    
-    if ~isempty(this_continuous_var)
-        continuous_var{i}.val = [this_continuous_var(:).val];
-        continuous_var{i}.ids = cell2mat({this_continuous_var(:).ids}')';
-        continuous_var{i}.name = this_continuous_tag;
-    else
-        continuous_var{i}.val = [];
-        continuous_var{i}.ids = [];
-        continuous_var{i}.name = this_continuous_tag;
-    end
-end
 end
